ace7b3de855a2e4518b8007b745cf4d0781583fa
creativekathryn <gretkatwei@gmail.com>
2024-09-08 23:45:07 -0500

    tried website version 2
    the end for this duedate

--- toggle.js
+++ toggle.js
@@ -1,9 +1,13 @@
-var num = 0;
+if (localStorage.getItem("theme") == "blue"){
+    style_two();
+}
 
 function style_one(){
     document.getElementById('page_style').setAttribute("href", "style1.css");
+    localStorage.setItem("theme", "purple");
 }
 
 function style_two(){
     document.getElementById('page_style').setAttribute("href", "style2.css");
+    localStorage.setItem("theme", "blue");
 }
