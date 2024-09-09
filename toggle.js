870b741c4c0e00d4ab455bb3b421f8c726b29d3f
creativekathryn <gretkatwei@gmail.com>
2024-09-08 22:19:36 -0500

    added toggle buttons

---
+++ toggle.js
@@ -0,0 +1,9 @@
+var num = 0;
+
+function style_one(){
+    document.getElementById('page_style').setAttribute("href", "style1.css");
+}
+
+function style_two(){
+    document.getElementById('page_style').setAttribute("href", "style2.css");
+}
